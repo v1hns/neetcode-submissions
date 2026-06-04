fa4057087dbfdfd7fb7591650cfa84c7c76e0acb
Vihaan Shringi <161313118+v1hns@users.noreply.github.com>
2026-06-04 10:21:26 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        n1 = 0
+        fib = []
+        while n1 <= n+1:
+            if len(fib) == 0: fib.append(0)
+            elif len(fib) == 1: fib.append(1)
+            else: fib.append(fib[n1-2]+fib[n1-1])
+            n1 += 1
+        print(fib)
+        return fib[n+1]
